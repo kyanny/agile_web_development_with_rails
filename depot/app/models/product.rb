2cc7deb9ea268371a1ca29fe91df89d225b62bc4
Kensuke Kaneko <kyanny@gmail.com>
2010-01-19 19:13:34 +0900

    Add validates

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,12 @@
+# -*- coding: utf-8 -*-
 class Product < ActiveRecord::Base
+    validates_presence_of :title, :description, :image_url
+    validates_numericality_of :price
+    validate :price_must_be_at_least_a_cent
+    validates_uniqueness_of :title
+    validates_format_of :image_url, :with => %r{\.(gif|jpe?g|png)$}i, :message => 'は GIF, JPG, PNG 画像の URL でなければなりません'
+
+    def price_must_be_at_least_a_cent
+        errors.add(:price, "は最小でも 0.01 以上でなければなりません") if price.nil? || price < 0.01
+    end
 end
