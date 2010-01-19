2a6611ab617a2c392790c1f69e502267808e49ad
Kensuke Kaneko <kyanny@gmail.com>
2010-01-19 19:03:41 +0900

    modify add_price_to_product migration

--- depot/db/migrate/20100119100141_add_price_to_product.rb
+++ depot/db/migrate/20100119100141_add_price_to_product.rb
@@ -1,6 +1,7 @@
 class AddPriceToProduct < ActiveRecord::Migration
   def self.up
-    add_column :products, :price, :decimal
+    add_column :products, :price, :decimal,
+      :precision => 8, :scale => 2, :default => 0
   end
 
   def self.down
