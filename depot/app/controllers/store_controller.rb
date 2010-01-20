9237ad0d8803168592a2457772b19c649dbbff23
Kensuke Kaneko <kyanny@gmail.com>
2010-01-21 00:31:22 +0900

    add_to_cart 実装

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -3,6 +3,12 @@ class StoreController < ApplicationController
       @products = Product.find_products_for_sale
   end
 
+    def add_to_cart
+        product = Product.find(params[:id])
+        @cart = find_cart
+        @cart.add_product(product)
+    end
+
     private
 
     def find_cart
