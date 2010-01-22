5a277c5d1c5daecf607a3f41622a5ba628dd71d0
Kensuke Kaneko <kyanny@gmail.com>
2010-01-22 15:16:59 +0900

    order.pay_type size limit

--- depot/db/migrate/20100122061403_create_orders.rb
+++ depot/db/migrate/20100122061403_create_orders.rb
@@ -4,7 +4,7 @@ class CreateOrders < ActiveRecord::Migration
       t.string :name
       t.text :address
       t.string :email
-      t.string :pay_type
+      t.string :pay_type, :limit => 10
 
       t.timestamps
     end
