c579ab8f52a360e63d4e7e0e85cf62f849357132
Kensuke Kaneko <kyanny@gmail.com>
2010-01-22 16:02:16 +0900

    define PAYMENT_TYPES in model

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 class Order < ActiveRecord::Base
     has_many :line_items
+    PAYMENT_TYPES = [
+        ["現金", "check"],
+        ["クレジットカード", "cc"],
+        ["注文書", "po"],
+    ]
 end
