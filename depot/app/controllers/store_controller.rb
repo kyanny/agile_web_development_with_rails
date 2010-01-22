04320567a1464e072f0bca1febe4bbbbc1d793a6
Kensuke Kaneko <kyanny@gmail.com>
2010-01-22 16:13:58 +0900

    save_order action を実装

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -40,6 +40,18 @@ class StoreController < ApplicationController
         end
     end
 
+    def save_order
+        @cart = find_cart
+        @order = Order.new(params[:order])
+        @order.add_line_items_from_cart(@cart)
+        if @order.save
+            session[:cart] = nil
+            redirect_to_index("ご注文ありがとうございます")
+        else
+            render :action => "checkout"
+        end
+    end
+
     private
 
     def find_cart
