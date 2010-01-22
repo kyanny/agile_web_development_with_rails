1984930e5092b2cae7cb0b42cfc42006492a0114
Kensuke Kaneko <kyanny@gmail.com>
2010-01-22 15:19:27 +0900

    line_items マイグレーション修正
    外部キー制約ほか

--- depot/db/migrate/20100122061534_create_line_items.rb
+++ depot/db/migrate/20100122061534_create_line_items.rb
@@ -1,10 +1,10 @@
 class CreateLineItems < ActiveRecord::Migration
   def self.up
     create_table :line_items do |t|
-      t.integer :product_id
-      t.integer :order_id
-      t.integer :quantity
-      t.decimal :total_price
+      t.integer :product_id, :null => false, :options => "CONSTRAINT fk_line_item_products REFERENCES products(id)"
+      t.integer :order_id, :null => false, :options => "CONSTRAINT fk_line_item_orders REFERENCES orders(id)"
+      t.integer :quantity, :null => false
+      t.decimal :total_price, :null => false, :precision => 8, :scale => 2
 
       t.timestamps
     end
