bb8580ae28b397d9fbb43d4c29a40b27ecc7e82b
Kensuke Kaneko <kyanny@gmail.com>
2010-01-25 23:47:11 +0900

    User モデルにバリデーションを追加

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
+
+    validates_presence_of :name
+    validates_uniqueness_of :name
+
+    attr_accessor :password_confirmation
+    validates_confirmation_of :password
+
+    validate :password_non_blank
+
+    private
+
+    def password_non_blank
+        errors.add(:password, "パスワードを入れてください") if hashed_password.blank?
+    end
 end
