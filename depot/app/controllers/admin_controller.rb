b74ab31aa0104cea70042b321d8f8df9f7373bba
Kensuke Kaneko <kyanny@gmail.com>
2010-01-26 01:50:19 +0900

    ログインフォームの実装

--- depot/app/controllers/admin_controller.rb
+++ depot/app/controllers/admin_controller.rb
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
 class AdminController < ApplicationController
   def login
+      if request.post?
+          user = User.authenticate(params[:name], params[:password])
+          if user
+              session[:user_id] = user.id
+              redirect_to(:action => 'index')
+          else
+              flash.now[:notice] = "無効なユーザ/パスワードの組み合わせです"
+          end
+      end
   end
 
   def logout
