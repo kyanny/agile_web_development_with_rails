b3c4896be56097b54be78e8366d6af8a57013117
Kensuke Kaneko <kyanny@gmail.com>
2010-01-26 02:01:52 +0900

    fix

--- depot/app/controllers/application.rb
+++ depot/app/controllers/application.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def authorize
-        unless User.find_by_id(:session[:user_id])
+        unless User.find_by_id(session[:user_id])
             flash[:notice] = "ログインしてください"
             redirect_to :controller => 'admin', :action => 'login'
         end
