f82ae74743791fe2eb132500db76224404e49ec2
Kensuke Kaneko <kyanny@gmail.com>
2010-01-26 01:57:51 +0900

    before_filter
    authorize メソッドを login をのぞくすべてのアクションの前にフックして実行

--- depot/app/controllers/application.rb
+++ depot/app/controllers/application.rb
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+    before_filter :authorize, :except => :login
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
@@ -12,4 +14,12 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
+
+    protected
+    def authorize
+        unless User.find_by_id(:session[:user_id])
+            flash[:notice] = "ログインしてください"
+            redirect_to :controller => 'admin', :action => 'login'
+        end
+    end
 end
